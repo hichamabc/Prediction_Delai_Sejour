68b1aa4fedf90e46c56229a57cd3bfb2027cf9d5
hichamabc <hichamamchtk@gmail.com>
2024-01-16 17:37:10 +0100

    Redctifying Ingestion data  code after testing

--- Ingestion_Data.py
+++ Ingestion_Data.py
@@ -5,10 +5,10 @@ from  datetime import datetime
 
 class Ingestion_Data :
     def __init__(self,path_1, path_2):
-        path_1= path_1
-        path_2=path_2
+        self.path_1= path_1
+        self.path_2=path_2
 
-    def diff_date(A,B):
+    def diff_date(self,A,B):
         date_A=datetime.strptime(A,'%d-%m-%y')
         date_B=datetime.strptime(B,'%d-%m-%y')
         date_diff=date_B-date_A
@@ -16,25 +16,26 @@ class Ingestion_Data :
         
         return diff_days
 
-    def Total_Df():
+    def Total_Df(self):
 
-        df1=pd.read_csv(path_1, sep=';', encoding='latin1')
+        df1=pd.read_csv(self.path_1, sep=';', encoding='latin1')
 
-        df2=pd.read_csv(path_2, sep=';', encoding='latin1')
+        df2=pd.read_csv(self.path_2, sep=';', encoding='latin1')
 
-        df=df1.merge(df2, left_on=('AVIS_ARRIVEE','NUM_BL'), right_on=('ID_AVISO_LLEGADA','NUM_BL'), how='inner')
+        A=df1.merge(df2, left_on=('AVIS_ARRIVEE','NUM_BL'), right_on=('ID_AVISO_LLEGADA','NUM_BL'), how='inner')
 
-        A['Delai_pointage_BAD']=list(map(diff_date,A['DATE_POINTAGE'],A['DATE_VALIDATION_BAD']))
+        A['Delai_pointage_BAD']=list(map(self.diff_date,A['DATE_POINTAGE'],A['DATE_VALIDATION_BAD']))
         
-        A['Delai_Creation_DUM_BAD']=list(map(diff_date,A['DATE_VALIDATION_BAD'],A['DATE_CREATION_DUM'])) 
+        A['Delai_Creation_DUM_BAD']=list(map(self.diff_date,A['DATE_VALIDATION_BAD'],A['DATE_CREATION_DUM'])) 
 
-        A=df.loc[:,['AVIS_ARRIVEE','NUM_BL','CODE_SH','LIEU_CHARGEMENT','CODE_LIEU_CHARGEMENT','PAYS_CHARGEMENT','PORT','TERMINAL','CODE_DOUANE','DATE_VALIDATION_BAD','DATE_CREATION_DUM','DATE_ML','DELAI_ML','ANNEE','MOIS','JOUR','DATE_POINTAGE','DELAI_SEJOUR']]
+        A=A.loc[:,['AVIS_ARRIVEE','NUM_BL','CODE_SH','LIEU_CHARGEMENT','CODE_LIEU_CHARGEMENT','PAYS_CHARGEMENT','PORT','TERMINAL','CODE_DOUANE','DATE_VALIDATION_BAD','Delai_Creation_DUM_BAD','Delai_pointage_BAD','DATE_CREATION_DUM','DATE_ML','DELAI_ML','ANNEE','MOIS','JOUR','DATE_POINTAGE','DELAI_SEJOUR']]
 
 
         return A
 
     
-    def Aggregate_Data(colum):
+    def Aggregate_Data(self,column):
+        A=self.Total_Df()
         df_column=A.loc[:,[column,'Delai_Creation_DUM_BAD','Delai_pointage_BAD','DELAI_ML','DELAI_SEJOUR']]
         df_column=df_column.groupby(column).median()
         df_column.reset_index(inplace=True)
@@ -42,7 +43,7 @@ class Ingestion_Data :
         return df_column
 
     
-    def data(list_column):
+    def data(self,list_column):
         A=self.Total_Df()
 
         for column in list_column:
--- Main.py
+++ Main.py
@@ -2,5 +2,13 @@ from Ingestion_Data import Ingestion_Data
 
 
 
+
+
 if __name__ == "__main__":
-    main()
+    file1='C:/Users/HAMCHTKOU/Documents/Prédiction_Délai_sejour/POC4/data.csv'
+    file2='C:/Users/HAMCHTKOU/Documents/Prédiction_Délai_sejour/POC4/délai_sejour_final.csv'
+
+    D=Ingestion_Data(file1,file2)
+    df=D.data(['CODE_SH','CODE_LIEU_CHARGEMENT','TERMINAL','CODE_DOUANE'])
+
+    df.to_csv('data.csv')
